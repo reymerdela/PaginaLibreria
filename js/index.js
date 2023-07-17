7e3ae0df831a20688b96308b9627b4f043acd13a
reymerdela <reymerdela@users.noreply.github.com>
2023-07-17 11:52:26 -0400

    ...

--- js/index.js
+++ js/index.js
@@ -1,6 +1,25 @@
+import { crearTarjeta } from "./tarjeta.js";
 const navBtn = document.getElementById("nav-btn");
 const navBar = document.getElementById("nav-bar");
 const heroImg = document.getElementById("hero-img");
+const btnAll = document.getElementById("btn-all");
+const cardContainer = document.getElementById("card-container");
+
+function generarTarjetas(){
+
+}
+crearTarjeta()
+
+
+function mostrarTodo(n){
+  btnAll.onclick = (ev)=>{
+    ev.preventDefault(); 
+  }
+}
+
+mostrarTodo(1)
+
+
 
 const intervaloHero = () => {
   let i = 2;
@@ -10,11 +29,13 @@ const intervaloHero = () => {
     if (i == 10) {
       i = 1;
     }
-  }, 4000);
+  }, 8000);
 }
 
 navBtn.addEventListener("click", () => {
 navBar.classList.toggle("aside-nav");
 });
 
+
+mostrarTodo();
 intervaloHero();
---
+++ js/tarjeta.js
@@ -0,0 +1,23 @@
+export function crearTarjeta(titulo, descripcion, imagenURL) {
+    const tarjeta = document.createElement('div');
+    tarjeta.classList.add('tarjeta');
+
+    const imagen = document.createElement('img');
+    imagen.src = imagenURL;
+    tarjeta.appendChild(imagen);
+
+    const contenido = document.createElement('div');
+    contenido.classList.add('contenido');
+
+    const tituloElemento = document.createElement('h2');
+    tituloElemento.textContent = titulo;
+    contenido.appendChild(tituloElemento);
+
+    const descripcionElemento = document.createElement('p');
+    descripcionElemento.textContent = descripcion;
+    contenido.appendChild(descripcionElemento);
+
+    tarjeta.appendChild(contenido);
+
+    return tarjeta;
+}
