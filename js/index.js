049a403da3ed713c5f20f5499f23c48e1059ff42
reymerdela <reymerdela@users.noreply.github.com>
2023-07-19 17:55:27 -0400

    ..

--- js/index.js
+++ js/index.js
@@ -1,41 +1,16 @@
-import { crearTarjeta } from "./tarjeta.js";
-const navBtn = document.getElementById("nav-btn");
-const navBar = document.getElementById("nav-bar");
+// import { crearTarjeta } from "./tarjeta.js";
+
 const heroImg = document.getElementById("hero-img");
-const btnAll = document.getElementById("btn-all");
-const cardContainer = document.getElementById("card-container");
-
-function generarTarjetas(){
-
-}
-crearTarjeta()
-
-
-function mostrarTodo(n){
-  btnAll.onclick = (ev)=>{
-    ev.preventDefault(); 
-  }
-}
-
-mostrarTodo(1)
-
-
 
 const intervaloHero = () => {
   let i = 2;
-  setInterval(() => {
-    heroImg.setAttribute("src", `./assets/libros/libro${i}.gif`);
-    i++;
-    if (i == 10) {
-      i = 1;
-    }
-  }, 8000);
-}
+    setInterval(() => {
+      heroImg.setAttribute("src", `/assets/libros/libro${i}.gif`);
+      i++;
+      if (i == 10) {
+        i = 1;
+      }
+    }, 4000);
+};
 
-navBtn.addEventListener("click", () => {
-navBar.classList.toggle("aside-nav");
-});
-
-
-mostrarTodo();
 intervaloHero();
--- js/tarjeta.js
+++ js/tarjeta.js
@@ -1,23 +1,23 @@
-export function crearTarjeta(titulo, descripcion, imagenURL) {
-    const tarjeta = document.createElement('div');
-    tarjeta.classList.add('tarjeta');
+// export function crearTarjeta(titulo, descripcion, imagen) {
+//     const tarjeta = document.createElement('div');
+//     tarjeta.classList.add('tarjeta');
 
-    const imagen = document.createElement('img');
-    imagen.src = imagenURL;
-    tarjeta.appendChild(imagen);
+//     const imagen = document.createElement('img');
+//     imagen.src = imagen;
+//     tarjeta.appendChild(imagen);
 
-    const contenido = document.createElement('div');
-    contenido.classList.add('contenido');
+//     const contenido = document.createElement('div');
+//     contenido.classList.add('contenido');
 
-    const tituloElemento = document.createElement('h2');
-    tituloElemento.textContent = titulo;
-    contenido.appendChild(tituloElemento);
+//     const tituloElemento = document.createElement('h2');
+//     tituloElemento.textContent = titulo;
+//     contenido.appendChild(tituloElemento);
 
-    const descripcionElemento = document.createElement('p');
-    descripcionElemento.textContent = descripcion;
-    contenido.appendChild(descripcionElemento);
+//     const descripcionElemento = document.createElement('p');
+//     descripcionElemento.textContent = descripcion;
+//     contenido.appendChild(descripcionElemento);
 
-    tarjeta.appendChild(contenido);
+//     tarjeta.appendChild(contenido);
 
-    return tarjeta;
-}
+//     return tarjeta;
+// }
